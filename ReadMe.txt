b09d9e81e69b12d9e9ffbd5cde02c700fa7c105b
Greig Paterson <Greig@Greigs-MacBook-Pro.local>
2016-02-11 12:44:31 +0800

    Initial Commit

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+This depository includes data and a MATLAB function for adjusting paleointensity site scatter for the number of specimens used to obtain the average result.
+This follow the work of Paterson et al. (2010)
+
+If any problems or questions arise please e-mail me (greig.paterson@mail.iggcas.ac.cn)
+
+
+References:
+Paterson, G. A., D. Heslop, and A. R. Muxworthy (2010), Deriving confidence in paleointensity estimates, Geochem. Geophys. Geosyst., 11, Q07Z18, doi:10.1029/2010GC003071.
---
+++ dB_n_adjust.m
@@ -0,0 +1,44 @@
+function [dBn]=dB_n_adjust(m, s, n, alpha)
+
+% Function to adjust the site scatter of a paleointensity estimate for
+% sample size following [1]. Please cite [1] if used.
+%
+% Note: This requires the Statistics Toolbox
+%
+% Written by Greig A. Paterson
+%
+% Input:
+%       m - vector of mean values
+%       s - vector of standard deviations
+%       n - vector of the number of specimens
+%
+% Output:
+%       dBn - the adjusted site scatter as a % of the mean
+%
+%
+% References:
+%       [1] Paterson, G. A., D. Heslop, and A. R. Muxworthy (2010), 
+%           Deriving confidence in paleointensity estimates, 
+%           Geochem. Geophys. Geosyst., 11, Q07Z18, doi:10.1029/2010GC003071.
+%
+
+%% Input checking
+if nargin < 3
+    error('dB_n_adjust:Input', 'At least 3 input arguments are required.');
+end
+
+if ~isequal(size(m), size(s), size(n))
+    error('dB_n_adjust:Input', 'm, s, and n must the same size.');
+end
+
+if nargin < 4
+    alpha=0.05;
+end
+
+%% Main function
+
+tnc = nctinv(alpha, (n-1), m.*sqrt(n)./s);
+
+dBn = 100.*abs(sqrt(n)./tnc);
+
+end
